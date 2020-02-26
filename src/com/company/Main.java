fa917be4a3dbe74ccb9f8d915f8a018749b1309f
Sadat <mukul_iiuc@yahoo.com>
2020-02-26 16:12:20 +0100

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
     }
+    public void add(int a,int b)
+    {
+        thi.
+        int b = a+b;
+        return (a+b);
+    }
+
+
 }
